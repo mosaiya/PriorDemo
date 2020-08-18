be17b14122d8cf8d25babf05efa8ce4fcd685fa5
mosaiya <pnoteuaja@gmail.com>
2020-08-18 19:45:47 +0700

    swager\

--- src/main/java/com/example/demo/modules/paramgroup/controller/ParamGroupController.java
+++ src/main/java/com/example/demo/modules/paramgroup/controller/ParamGroupController.java
@@ -1,9 +1,14 @@
 package com.example.demo.modules.paramgroup.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.ParamGroup;
 import com.example.demo.modules.paramgroup.services.ParamGroupService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,5 +21,9 @@ public class ParamGroupController {
 	@Autowired
 	private ParamGroupService groupService ;
 	
-	
+	@GetMapping("/inquiry/{paramgroup}")
+	public List<ParamGroup> inquiry(@PathVariable   Long paramgroup ){
+		List<ParamGroup> paramGroups = groupService.InquiryParamGroup(paramgroup);
+		return paramGroups ;
+	}
 }
--- src/main/java/com/example/demo/modules/paramgroup/services/ParamGroupService.java
+++ src/main/java/com/example/demo/modules/paramgroup/services/ParamGroupService.java
@@ -1,8 +1,12 @@
 package com.example.demo.modules.paramgroup.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.ParamGroup;
 import com.example.demo.repository.ParameterGroupRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,4 +17,17 @@ public class ParamGroupService {
 		
 		@Autowired
 		private ParameterGroupRepository groupRepository ;
+		
+		public List<ParamGroup> InquiryParamGroup(Long paramGroup) {
+			List<ParamGroup> cpipMsParameterGroups = new ArrayList<>();
+			try {
+				 cpipMsParameterGroups = groupRepository.findByParamGroupIdAndIsDeleted(paramGroup, "N");
+				
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				e.printStackTrace();
+			}
+			
+			return cpipMsParameterGroups ;
+		}
 }
